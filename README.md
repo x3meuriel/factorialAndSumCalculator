a665e0ac7c105fe6a0c2346d8728c17432bea513
x3meuriel <x3me7@hotmail.com>
2021-11-25 21:20:17 -0600

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-
-# factorialAndSumCalculator
-a calculator that calculates the factorial and recursiveSum(from N +  N-1 + N-2 + ....  + N-N) of a number  
---
+++ precache-manifest.b9aabe61186940c8d0afa448ca030dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f7a49d33ebaa5f855cc8ef86e8baa5",
+    "url": "/factorialAndSumCalculator/index.html"
+  },
+  {
+    "revision": "842101f1d919bdfcfdad",
+    "url": "/factorialAndSumCalculator/static/css/main.2806e284.chunk.css"
+  },
+  {
+    "revision": "e91f463151a2479c0e76",
+    "url": "/factorialAndSumCalculator/static/js/2.223e9d93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/factorialAndSumCalculator/static/js/2.223e9d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842101f1d919bdfcfdad",
+    "url": "/factorialAndSumCalculator/static/js/main.e813dbde.chunk.js"
+  },
+  {
+    "revision": "998961c91a5c382e79f1",
+    "url": "/factorialAndSumCalculator/static/js/runtime-main.23cc724b.js"
+  },
+  {
+    "revision": "bca0cd3d5393f02359eff04e768224c9",
+    "url": "/factorialAndSumCalculator/static/media/background.bca0cd3d.jpg"
+  }
+]);
